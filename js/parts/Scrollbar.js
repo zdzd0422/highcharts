5e6bb30056b3d3901eba9799458504aad2e1db39
Paweł Fus <pawel.fus.88@gmail.com>
2017-03-02 16:42:00 +0100

    Revert "Highstock: Fixed #6421, errors after destroying a chart with multiple scrollbars."
    This reverts commit b6db6b0c3c419da042345eeb8c423a174a15a5c0.

--- js/parts/Scrollbar.js
+++ js/parts/Scrollbar.js
@@ -624,7 +624,7 @@ Scrollbar.prototype = {
 			}
 		}, this);
 
-		if (scroller && this == scroller) { // #6421, chart may have more scrollbars
+		if (scroller) {
 			scroller.scrollbar = null;
 
 			// Destroy elements in collection
