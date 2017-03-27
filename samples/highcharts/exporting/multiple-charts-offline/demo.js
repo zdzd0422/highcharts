3986e4bbc3ffe620c8f32b96908165688f41d0e6
oysteinmoseng <oystein@highsoft.com>
2017-03-27 14:10:14 +0200

    Fixed #6499, offline export fails with allowHTML despite no HTML in chart.

--- samples/highcharts/exporting/multiple-charts-offline/demo.js
+++ samples/highcharts/exporting/multiple-charts-offline/demo.js
@@ -1,45 +1,3 @@
-<<<<<<< HEAD
-$(function () {
-
-    /**
-     * Create a global getSVG method that takes an array of charts as an argument. The SVG is returned as an argument in the callback.
-     */
-    Highcharts.getSVG = function (charts, options, callback) {
-        var svgArr = [],
-            top = 0,
-            width = 0,
-            addSVG = function (svgres) {
-                // Grab width/height from exported chart
-                var svgWidth = +svgres.match(
-                        /^<svg[^>]*width\s*=\s*\"?(\d+)\"?[^>]*>/
-                    )[1],
-                    svgHeight = +svgres.match(
-                        /^<svg[^>]*height\s*=\s*\"?(\d+)\"?[^>]*>/
-                    )[1],
-                    // Offset the position of this chart in the final SVG
-                    svg = svgres.replace('<svg', '<g transform="translate(0,' + top + ')" ');
-                svg = svg.replace('</svg>', '</g>');
-                top += svgHeight;
-                width = Math.max(width, svgWidth);
-                svgArr.push(svg);
-            },
-            exportChart = function (i) {
-                if (i === charts.length) {
-                    return callback('<svg height="' + top + '" width="' + width +
-                      '" version="1.1" xmlns="http://www.w3.org/2000/svg">' + svgArr.join('') + '</svg>');
-                }
-                charts[i].getSVGForLocalExport(options, {}, function () {
-                    console.log("Failed to get SVG");
-                }, function (svg) {
-                    addSVG(svg);
-                    return exportChart(i + 1); // Export next only when this SVG is received
-                });
-            };
-        exportChart(0);
-    };
-=======
-
-
 /**
  * Create a global getSVG method that takes an array of charts as an argument. The SVG is returned as an argument in the callback.
  */
@@ -76,7 +34,6 @@ Highcharts.getSVG = function (charts, options, callback) {
         };
     exportChart(0);
 };
->>>>>>> master
 
 /**
  * Create a global exportCharts method that takes an array of charts as an argument,
